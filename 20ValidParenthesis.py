44b30b5e8cdbcec9ea44dd9687f24499dd998e89
Sibel DeShong <sibeldeshong@gmail.com>
2019-09-19 10:43:49 -0400

    added improved comments

--- 20ValidParenthesis.py
+++ 20ValidParenthesis.py
@@ -1,6 +1,6 @@
 # This is my original, uneditted solution for this problem
 # 
-# Problem 20: Valid Parenthesis
+# Problem 20: Valid Parentheses
 # Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
 #
 # An input string is valid if:
@@ -19,11 +19,12 @@
 
 class Solution:
     def isValid(self, s: str) -> bool:
-        # condition for whether more parenthesis were taken out in the loop or not
+        # condition for whether more parentheses were taken out in the loop or not
         paren_taken_out = True
         
+        # remove complete brackets if they exist
+        # loop continues until an interation where no parentheses are taken out from string s
         while paren_taken_out == True:
-            # iterate through characters in string
             paren_taken_out = False
             if "[]" in s:
                 s = s.replace("[]", "")
@@ -36,7 +37,10 @@ class Solution:
             if "{}" in s:
                 s = s.replace("{}", "")
                 paren_taken_out = True
-            
+        
+        # loop has been broken, now if string still contains a parenthese
+        # then return false because it is not valid
+        # if string has no parenthese returns true for valid string
         if paren_taken_out == False:
             if "(" in s or ")" in s or "[" in s or "]" in s or '{' in s or "}" in s:
                 return False
